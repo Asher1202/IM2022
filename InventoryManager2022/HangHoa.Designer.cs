6c9c02ec3bad52eafcec29754328595a2ca383aa
Asher1202 <rainbowasher2002@gmail.com>
2022-10-13 04:29:19 +0700

    Added thống kê form, hàng hóa form, main menu form

---
+++ InventoryManager2022/HangHoa.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace InventoryManager2022
+{
+    partial class HangHoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gb_ThongTin = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.iconButton2 = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            this.gb_ThongTin.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.gb_ThongTin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // gb_ThongTin
+            // 
+            this.gb_ThongTin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(178)))), ((int)(((byte)(255)))));
+            this.gb_ThongTin.Controls.Add(this.iconButton2);
+            this.gb_ThongTin.Controls.Add(this.comboBox1);
+            this.gb_ThongTin.Controls.Add(this.textBox3);
+            this.gb_ThongTin.Controls.Add(this.textBox2);
+            this.gb_ThongTin.Controls.Add(this.textBox1);
+            this.gb_ThongTin.Controls.Add(this.label4);
+            this.gb_ThongTin.Controls.Add(this.label3);
+            this.gb_ThongTin.Controls.Add(this.label2);
+            this.gb_ThongTin.Controls.Add(this.label1);
+            this.gb_ThongTin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gb_ThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.gb_ThongTin.Location = new System.Drawing.Point(0, 0);
+            this.gb_ThongTin.Name = "gb_ThongTin";
+            this.gb_ThongTin.Size = new System.Drawing.Size(330, 450);
+            this.gb_ThongTin.TabIndex = 0;
+            this.gb_ThongTin.TabStop = false;
+            this.gb_ThongTin.Text = "Nhập hàng hóa";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(6, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên hàng hóa :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label2.Location = new System.Drawing.Point(6, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số lượng nhập :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label3.Location = new System.Drawing.Point(6, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhà cung cấp :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label4.Location = new System.Drawing.Point(6, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(161, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Loại hàng hóa :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox1.Location = new System.Drawing.Point(164, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 31);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox2.Location = new System.Drawing.Point(164, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 31);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.textBox3.Location = new System.Drawing.Point(164, 168);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(160, 31);
+            this.textBox3.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(332, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(468, 450);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(330, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2, 450);
+            this.panel2.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Hàng khô",
+            "Hàng thịt",
+            "Hàng đông lạnh",
+            "Hàng giải khát"});
+            this.comboBox1.Location = new System.Drawing.Point(164, 211);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 33);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // iconButton2
+            // 
+            this.iconButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.iconButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(177)))), ((int)(((byte)(178)))), ((int)(((byte)(255)))));
+            this.iconButton2.IconChar = FontAwesome.Sharp.IconChar.Plus;
+            this.iconButton2.IconColor = System.Drawing.Color.Black;
+            this.iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton2.IconSize = 30;
+            this.iconButton2.Location = new System.Drawing.Point(12, 273);
+            this.iconButton2.Name = "iconButton2";
+            this.iconButton2.Size = new System.Drawing.Size(92, 28);
+            this.iconButton2.TabIndex = 8;
+            this.iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // HangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "HangHoa";
+            this.Text = "HangHoa";
+            this.panel1.ResumeLayout(false);
+            this.gb_ThongTin.ResumeLayout(false);
+            this.gb_ThongTin.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox gb_ThongTin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private FontAwesome.Sharp.IconButton iconButton2;
+    }
+}
---
+++ InventoryManager2022/formMainMenu.cs
@@ -0,0 +1,136 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManager2022
+{
+    public partial class formMainMenu : Form
+    {
+        //Khai báo biến cho panel và button màu
+        private IconButton buttonChon;
+        private Panel leftPanel;
+        private Form formOpen;
+        public formMainMenu()
+        {
+            InitializeComponent();
+            leftPanel = new Panel();
+            leftPanel.Size = new Size(7, 51);
+            panelMenu.Controls.Add(leftPanel);
+            tuyChinhMenu();
+        }
+        //callbacks
+
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24, 161, 251);
+        }
+        private void OpenChildForm(Form childForm)
+        {
+            //open only form
+            if (formOpen != null)
+            {
+                formOpen.Close();
+            }
+            formOpen = childForm;
+            //End
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelShow.Controls.Add(childForm);
+            panelShow.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+           // lb_choosed.Text = childForm.Text;
+        }
+        //method
+        private void tuyChinhMenu()
+        {
+            panelSubHangHoa.Visible = false;
+        }
+        private void anMenuCon()
+        {
+            if (panelSubHangHoa.Visible == true)
+                panelSubHangHoa.Visible = false;
+
+        }
+        private void hienMenuCon(Panel menuCon)
+        {
+            if (menuCon.Visible == false)
+            {
+                anMenuCon();
+                menuCon.Visible = true;
+            }
+            else menuCon.Visible = false;
+            
+
+        }
+        private void daNhanNut(object senderBtn, Color color)//active
+        {
+            if (senderBtn != null)
+            {
+                hetNhanNut();
+                buttonChon = (IconButton)senderBtn;
+                buttonChon.BackColor = Color.FromArgb(177, 178, 255);
+                buttonChon.ForeColor = color;
+                buttonChon.TextAlign = ContentAlignment.MiddleCenter;
+                buttonChon.IconColor = color;
+                buttonChon.TextImageRelation = TextImageRelation.TextBeforeImage;
+                buttonChon.ImageAlign = ContentAlignment.MiddleRight;
+                // khu vuc ben trai button
+                leftPanel.BackColor = color;
+                leftPanel.Location = new Point(0, buttonChon.Location.Y);
+                leftPanel.Visible = true;
+                leftPanel.BringToFront();
+                iconChild.IconChar = buttonChon.IconChar;
+                lb_choosed.Text = buttonChon.Text;
+
+            }
+        }
+        private void hetNhanNut()//deactive
+        {
+            if (buttonChon != null)
+            {
+                buttonChon.BackColor = Color.FromArgb(177, 178, 255);
+                buttonChon.ForeColor = Color.FromArgb(0,0,0); ;
+                buttonChon.TextAlign = ContentAlignment.MiddleCenter;
+                buttonChon.IconColor = Color.FromArgb(0, 0, 0); ;
+                buttonChon.TextImageRelation = TextImageRelation.ImageBeforeText;
+                buttonChon.ImageAlign = ContentAlignment.MiddleLeft;
+
+            }
+        }
+        private void btn_thongke_Click(object sender, EventArgs e)
+        {
+            daNhanNut(sender, RGBColors.color1);
+            OpenChildForm(new formThongKe());
+        }
+
+        private void btn_hanghoa_Click(object sender, EventArgs e)
+        {
+            daNhanNut(sender, RGBColors.color2);
+            hienMenuCon(panelSubHangHoa);
+        }
+
+        private void btn_nhanvien_Click(object sender, EventArgs e)
+        {
+            daNhanNut(sender, RGBColors.color3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
